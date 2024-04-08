e8024d0815d6ed16af723234317470de8e378cbb
johnmariita <johnmariita421@gmail.com>
2024-04-09 01:17:24 +0300

    size of a tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,10 +7,7 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t count = 0;
-
 	if (!tree)
 		return (0);
-	count = binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1;
-	return (count);
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
