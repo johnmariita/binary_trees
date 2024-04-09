ce0706552ad1f7da83c0e0dd68fa5e876dcd992c
johnmariita <johnmariita421@gmail.com>
2024-04-09 03:27:37 +0300

    function that resturns the number of leaves in a tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 
 /**
- *
- *
+ * binary_tree_leaves - function that rturns the number of leaves in a tree
+ * @tree: pointer to the tree
+ * Return: returns the number of leaves in the tree
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
@@ -11,5 +12,6 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 		return (0);
 	binary_tree_leaves(tree->left);
 	binary_tree_leaves(tree->right);
-	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right) + ((tree->left || tree->right) ? 0 : 1));
+	return (binary_tree_leaves(tree->left) +
+			binary_tree_leaves(tree->right) + ((tree->left || tree->right) ? 0 : 1));
 }
