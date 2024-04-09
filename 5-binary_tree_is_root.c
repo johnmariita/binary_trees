337ac75d8f039560572e5bb50e860d1d3fac0f35
johnmariita <johnmariita421@gmail.com>
2024-04-09 03:26:39 +0300

    Slight changes to make the code compliant with betty style

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,8 +1,9 @@
 #include "binary_trees.h"
 
 /**
- *
- *
+ * binary_tree_is_root - function to check if a node is a root
+ * @node: pointer to the node
+ * Return: returns 0 or 1 depending on whether the node is root
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
