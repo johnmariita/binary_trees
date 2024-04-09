a402dd8f3c5b30ef34d7b989543f6a153a8579e5
johnmariita <johnmariita421@gmail.com>
2024-04-09 03:28:21 +0300

    function that returns the nuber of nodes with atleast on child

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,10 +1,17 @@
 #include "binary_trees.h"
 
 /**
- *
- *
+ * binary_tree_nodes - function that returns the number of nodes
+ * with atleast one child
+ * @tree: pointer to the tree
+ * Return: returns the number of nodes with atleast one child
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-
+	if (!tree)
+		return (0);
+	binary_tree_nodes(tree->left);
+	binary_tree_nodes(tree->right);
+	return (binary_tree_nodes(tree->left) +
+			binary_tree_nodes(tree->right) + ((tree->left || tree->right) ? 1 : 0));
 }
