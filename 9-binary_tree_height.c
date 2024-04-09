7bb9b6c0048449b5e9141386be36a24656c9bd65
johnmariita <johnmariita421@gmail.com>
2024-04-09 17:32:45 +0300

    height of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,10 +15,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	lheight = binary_tree_height(tree->left) + (tree->left ? 1 : 0);
 	rheight = binary_tree_height(tree->right) + (tree->right ? 1 : 0);
-
-	/*if (lheight > rheight)
-		return (tree->left ? lheight + 1 : 0);
-	else
-		return (tree->right ? rheight + 1 : 0);*/
 	return (lheight > rheight ? lheight : rheight);
 }
